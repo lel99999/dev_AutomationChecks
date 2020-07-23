52e998d740742ea02ead12a3bb89819e956eac8a
lel99999 <34351721+lel99999@users.noreply.github.com>
2020-07-22 21:57:25 -0400

    Update dircheck.sh

--- dircheck.sh
+++ dircheck.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
-if [[ -f /home/<path>/<user>/.bashrc ]]
-then
-  echo "File exists on the filesystem"
+for i in $(ls -d /home/<user_specific_path>/*)
+do
+echo ${i%%/};
+if [[ -f $i/.bashrc ]]; then
+  echo $i >> /tmp/bashrc_exists.txt
+else
+  echo $i >> /tmp/bashrc_noexists.txt
 fi
-
+done
