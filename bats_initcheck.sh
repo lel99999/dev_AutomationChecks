6230870be57c71a811b7ef69487a37b6f87fc5fb
lel99999 <luan.le@cfpb.gov>
2021-06-24 23:08:11 -0400

    add bats

---
+++ bats_initcheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git clone https://github.com/sstephenson/bats /usr/local/bats
+
+## Simple test
+#!/usr/bin/env bats
+
+@test "addition using bc" {
+  result="$(echo 2+2 | bc)"
+  [ "$result" -eq 4 ]
+}
+
+@test "addition using dc" {
+  result="$(echo 2 2+p | dc)"
+  [ "$result" -eq 4 ]
+}
