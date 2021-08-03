b75c3f7c153c56a6256d301689ce2f9c1056a2e2
lel99999 <luan.le@cfpb.gov>
2021-08-02 23:41:20 -0400

    upd sys storage test

--- PyTest/FullSys_Tests/system_tests/test_system_storage.py
+++ PyTest/FullSys_Tests/system_tests/test_system_storage.py
@@ -1,5 +1,19 @@
+import os.path
+from os import path
 # get storage mounts from /etc/fstab, check if valid
 # run basic latency test of read/write
+testpaths = ["/tmp","/Users"]
+def checkpath(_path):
+#   isFile = os.path.isfile(path)
+#   isDir = os.path.isdir(path)
+    Path_exists = path.isdir(_path)
+    return Path_exists
 
 def test_storage_mounts():
-    assert 1 == 1 
+#   assert 1 == 1 
+    for ckpath in testpaths:
+        print(ckpath + " : " + str(checkpath(ckpath)))
+
+
+if __name__ == "__main__":
+    test_storage_mounts()
