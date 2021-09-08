cd699f8db592433a507ea90f530f4b09d8205d9a
lel99999 <luan.le@cfpb.gov>
2021-09-08 14:24:15 -0400

    upd tf

--- PyTest/FullSys_Tests/runTestsFabric.py
+++ PyTest/FullSys_Tests/runTestsFabric.py
@@ -1,7 +1,10 @@
 from fabric import Connection
 
+_hostname = wdcreeodl02
+_uid = "deploy"
+_hoststring = _uid + "@" + _hostname
 cmdClone = "git clone https://github.com/lel99999/dev_AutomationChecks.git /tmp/autocheck"
-testresult = Connection('deploy@wdcreeodl02').run(cmdClone,hide=True)
+testresult = Connection(_hoststring).run(cmdClone,hide=True)
 msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
 outfile = open("/tmp/testRun.txt","w")
 outfile.write(msg)
