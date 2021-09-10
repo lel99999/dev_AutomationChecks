4f3b9adbcab2b39b123166510bbe892444fe91a5
lel99999 <luan.le@cfpb.gov>
2021-09-09 23:38:09 -0400

    fix path error

--- PyTest/FullSys_Tests/runTestsFabric.py
+++ PyTest/FullSys_Tests/runTestsFabric.py
@@ -43,7 +43,7 @@ cmdModCacheDirPerm = "chmod -R 777 " + _tmpPath + "/PyTest/FullSys_Tests"
 
 cmdPipReq = "pip3 install -r " + _tmpPath + "/PyTest/FullSys_Tests/requirements.txt"
 cmdPyTest = "pytest"
-cmdPyTest_wReport = "/tmp/venv3/bin/pytest " + _tmpPath + "/PyTest/FullSys_Tests/requirements.txt" + " --html-report=/tmp/autocheck/report/testReport.html"
+cmdPyTest_wReport = "/tmp/venv3/bin/pytest " + _tmpPath + "/PyTest/FullSys_Tests/" + " --html-report=/tmp/autocheck/report/testReport.html"
 
 cmdRun(cmdCleanDir)
 cmdRun(cmdClone)
