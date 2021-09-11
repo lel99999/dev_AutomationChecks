7659fe2be4122be13ab5bd6528509e153ae1e067
lel99999 <luan.le@cfpb.gov>
2021-09-10 21:54:20 -0400

    upd storage

--- PyTest/FullSys_Tests/custom_tests/test_system_storage.py
+++ PyTest/FullSys_Tests/custom_tests/test_system_storage.py
@@ -1,5 +1,7 @@
 import os.path
 from os import path
+import subprocess
+
 # get storage mounts from /etc/fstab, check if valid
 # run basic latency test of read/write
 testpaths = ["/tmp","/Users"]
@@ -14,6 +16,20 @@ def test_storage_mounts():
     for ckpath in testpaths:
         print(ckpath + " : " + str(checkpath(ckpath)))
 
+def test_knowndirs():
+    name = ["/tmp","/Users"]
+
+def test_storage_usage():
+    """ Check storage capcity - verify under a certain utilization % """    
+    # for Python 2.x
+#   output = subprocess.check_output(['df',-k'])
+
+    # for Python > v3.5
+    output = subprocess.run(['df','-k'],capture_output=True, text=True).stdout
+
+    #### run $pytest -s  ... to show printed output
+    print(output)
+    assert 1 == 1
 
 if __name__ == "__main__":
     test_storage_mounts()
