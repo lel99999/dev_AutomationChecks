182bde4ce51423a8456d82e860217ab589506d1d
lel99999 <luan.le@cfpb.gov>
2021-09-22 11:19:05 -0400

    upd testfab

--- PyTest/FullSys_Tests/runTestsFabric.py
+++ PyTest/FullSys_Tests/runTestsFabric.py
@@ -45,7 +45,7 @@ cmdModCacheDirPerm = "chmod -R 777 " + _tmpPath + "/PyTest/FullSys_Tests"
 
 cmdPipReq = "pip3 install -r " + _tmpPath + "/PyTest/FullSys_Tests/requirements.txt"
 cmdPyTest = "pytest"
-cmdPyTest_wReport = "/tmp/venv3/bin/pytest " + _tmpPath + "/PyTest/FullSys_Tests/system_tests/" + " --html-report=/tmp/autocheck/report/testReport.html"
+cmdPyTest_wReport = "/tmp/venv3/bin/pytest " + _tmpPath + "/PyTest/FullSys_Tests/custom_tests/" + " --html-report=/tmp/autocheck/report/testReport.html"
 
 cmdRun(cmdCleanDir)
 cmdRun(cmdClone)
