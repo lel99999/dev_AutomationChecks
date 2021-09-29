5caefe412fbc6852d43fe6d5fd12cdb7901acdb0
lel99999 <luan.le@cfpb.gov>
2021-09-29 10:23:49 -0400

    upd storage tests

--- PyTest/FullSys_Tests/system_tests/test_system_storage.py
+++ PyTest/FullSys_Tests/system_tests/test_system_storage.py
@@ -1,5 +1,7 @@
+import pytest
 import os.path
 from os import path
+
 # get storage mounts from /etc/fstab, check if valid
 # run basic latency test of read/write
 testpaths = ["/tmp","/Users"]
@@ -9,9 +11,16 @@ def checkpath(_path):
     Path_exists = path.isdir(_path)
     return Path_exists
 
-_mounts = ["/home/data","/home/work","/home/work2"]
+@pytest.mark.parametrize('_diskmount,_expected',[('/home/data',True),('/home/work',True),('/home/work2',True)])
+#@pytest.mark.parametrize('_diskmount,_expected',[('/Applications',True),('/System',True),('/Users',True),('/Users1',True)])
+def test_shared_mounts(_diskmount,_expected):
+    assert checkpath(_diskmount) == _expected
+
+#@pytest.mark.parametrize('_diskmount,_expected',[('/home/data',True),('/home/work',True),('/home/work2',True)])
 def test_storage_mounts():
     """ Check mounts /home/data, /home/work, /home/work2 """
+    _mounts = ["/home/data","/home/work","/home/work2"]
+#   _mounts = ["/Applications","/System","/Users"]
 #   assert 1 == 1 
     for ckmount in _mounts:
 #       print(ckmount)
