6c82ce2d03f1a87d8db20c0c42d150ea872a63e5
lel99999 <luan.le@cfpb.gov>
2021-09-29 03:25:49 -0400

    mod4 error handling

--- PyTest/FullSys_Tests/runTestsFabric.py
+++ PyTest/FullSys_Tests/runTestsFabric.py
@@ -9,13 +9,13 @@ _tmpPath = "/tmp/autocheck"
 _tmpVenv3Path = "/tmp/venv3"
 
 def cmdRun(runcmd):
-_cmd = runcmd
+    _cmd = runcmd
     try:
         _tmpStdOut = Connection(_hoststring).run(_cmd,hide=True).stdout.strip()
         msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
     except:
         print("Error: ", sys.exc_info()[0], "occurred.")
-print("--- " + _cmd)
+    print("--- " + _cmd)
 # print("--- " + _tmpStdOut)
       
 # outfile = open("/tmp/testRun.txt","w")
