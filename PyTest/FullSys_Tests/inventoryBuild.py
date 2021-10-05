0cd9950d08f57baafa78dc8cfb9134f245199941
lel99999 <luan.le@cfpb.gov>
2021-10-04 22:45:08 -0400

    add testInv file

--- PyTest/FullSys_Tests/inventoryBuild.py
+++ PyTest/FullSys_Tests/inventoryBuild.py
@@ -1,8 +1,11 @@
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 
-_inventory_file = '/tmp/inventory'
+#_inventory_file = '/tmp/inventory'
+_inventory_file = 'testInventory.txt'
 _dataloader = DataLoader()
 _inventory = InventoryManager(loader = _dataloader,sources=[_inventory_file])
 
-print(_inventory.get_groups_dict())
+#print(_inventory.get_groups_dict())
+#print(_inventory.get_groups_dict()['all'])
+print(_inventory.get_groups_dict()['prod'])
