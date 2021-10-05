2d3ccc7492a6f13129de838e12f4ed0ba3f3f185
lel99999 <luan.le@cfpb.gov>
2021-10-04 22:42:13 -0400

    upd invb

--- PyTest/FullSys_Tests/inventoryBuild.py
+++ PyTest/FullSys_Tests/inventoryBuild.py
@@ -5,35 +5,4 @@ _inventory_file = '/tmp/inventory'
 _dataloader = DataLoader()
 _inventory = InventoryManager(loader = _dataloader,sources=[_inventory_file])
 
-#variable_manager = VariableManager()
-#loader = DataLoader()
-
-#  Ansible: Load inventory
-#inventory = Inventory(
-#    loader = loader,
-#    variable_manager = variable_manager,
-#    host_list = '/tmp/inventory'
-#)
-
-
-def serialize(inventory):
-    if not isinstance(inventory, Inventory):
-        return dict()
-
-    data = list()
-    for group in inventory.get_groups():
-        if group != 'all':
-            group_data = inventory.get_group(group).serialize()
-
-            #  Seed host data for group
-            host_data = list()
-            for host in inventory.get_group(group).hosts:
-                host_data.append(host.serialize())
-
-            group_data['hosts'] = host_data
-            data.append(group_data)
-
-    return data
-
-serialized_inventory = serialize(_inventory)
-print(serialized_inventory)
+print(_inventory.get_groups_dict())
