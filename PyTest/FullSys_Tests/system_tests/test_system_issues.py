d1934321084efc64161a883f4e920ac50f897b45
lel99999 <luan.le@cfpb.gov>
2021-10-14 23:31:02 -0400

    add dict list tester

--- PyTest/FullSys_Tests/system_tests/test_system_issues.py
+++ PyTest/FullSys_Tests/system_tests/test_system_issues.py
@@ -1,5 +1,15 @@
 import pytest
 import os.path
+from collections import defaultdict
+
+_servers_svcs = [("server1","check port"),("server1","check path"),("server2","check service"),("server3","check path"),("server4","check path"),("server5","check path"),("server5","check service")]
+_dictServersSvcs = defaultdict(list)
+
+for k,v in _servers_svcs:
+    _dictServersSvcs[k].append(v)
+
+print("Dictionary of servers and list of svcs to check: ",_dictServersSvcs)
+
 
 #@pytest.mark.sas
 def test_checktmpdir():
