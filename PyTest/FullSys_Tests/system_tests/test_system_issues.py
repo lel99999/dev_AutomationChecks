9d376990a7829eb2e3b19e08196e357ae4a16bc9
lel99999 <luan.le@cfpb.gov>
2021-10-14 23:58:42 -0400

    add dic list3

--- PyTest/FullSys_Tests/system_tests/test_system_issues.py
+++ PyTest/FullSys_Tests/system_tests/test_system_issues.py
@@ -8,7 +8,7 @@ _dictServersSvcs = defaultdict(list)
 for k,v in _servers_svcs:
     _dictServersSvcs[k].append(v)
 
-print("Dictionary of servers and list of svcs to check: ", _dictServersSvcs)
+print("(1) Dictionary of servers and list of svcs to check: ", _dictServersSvcs)
 
 _dict_servers_svcs = {}
 _dict_servers_svcs["server1"] = ["check port","check path","check dir","check service"]
@@ -17,7 +17,24 @@ _dict_servers_svcs["server3"] = ["check service"]
 _dict_servers_svcs["server4"] = ["check path","check dir"]
 _dict_servers_svcs["server5"] = ["check service","check path","check port"] 
 
-print("Dictionary of servers/checks lists: ", _dict_servers_svcs)
+print("(2) Dictionary of servers/checks lists: ", _dict_servers_svcs)
+
+_dictServers = {}
+_dictServers["server1"] = ["check port","check path"]
+_dictServers["server2"] = ["check port","check service","check path"]
+_dictServers["server3"] = ["check service","check port"]
+_dictServers["server4"] = ["check service"]
+_dictServers["server5"] = ["check path","check service"]
+
+_addSvcChecks1 = ["check user","check acl"]
+_addSvcChecks2 = ["check service account","check enabled"]
+_addSvcChecks3 = ["check fw type","check fw rule"]
+
+_dictServers["server1"].append(_addSvcChecks2)
+_dictServers["server3"].append(_addSvcChecks1)
+_dictServers["server5"].append(_addSvcChecks3)
+
+print("(3) Dictionary of servers/checks lists: ", _dictServers)
 
 
 #@pytest.mark.sas
