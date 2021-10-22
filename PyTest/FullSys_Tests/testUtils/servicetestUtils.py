f5ee6c85bb1d26116f3c5138f6d8295da99b0b16
lel99999 <luan.le@cfpb.gov>
2021-10-21 22:34:05 -0400

    upd svc test

--- PyTest/FullSys_Tests/testUtils/servicetestUtils.py
+++ PyTest/FullSys_Tests/testUtils/servicetestUtils.py
@@ -1,11 +1,25 @@
 import requests
+import pytest
 
-def test_GET_web(aURL):
-    _r = requests.get(aURL)
-    return _r.text
+@pytest.fixture
+def aURL():
+    return "https://www.google.com"
 
-def test_POST_web(aURL):
-    pass
+#aURL = "https://www.google.com"
+def test_req_res(aURL):
+    _res = requests.get(aURL)
+    print(_res.ok)
+    assert(_res.ok) == True
 
-print(test_GET_web("https://www.google.com"))
+# API Tests
+#def test_GET_web(aURL):
+#    _res = requests.get(aURL)
+#    return _res.text
+
+#def test_POST_web(aURL):
+#    pass
+
+#print(test_req_res("https://www.google.com"))
+
+#print(test_GET_web("https://www.google.com"))
 
