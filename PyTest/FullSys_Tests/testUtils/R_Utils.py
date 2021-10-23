d6fb4b4ed2c80128549312d0771dd7e62ecc7826
lel99999 <luan.le@cfpb.gov>
2021-10-22 23:04:24 -0400

    add r utils file

---
+++ PyTest/FullSys_Tests/testUtils/R_Utils.py
@@ -0,0 +1,9 @@
+# R Related Testing Functions
+
+def test_r_postgresql(aServer):
+#>con <- dbConnect(RPostgres::Postgres(),dbname = '<dbname>', host = '<host>', port = '5432', user = '<uid>', password = '*****')
+#> census_q <- dbSendQuery(con,'SELECT * from public.test;')
+#> dbFetch(census_q) 
+
+def test_r_odbc(aServer):
+    pass
